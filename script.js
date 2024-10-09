2f79cc16ab316340a5142837bf270dbebe1b4585
Omgaikwadb <111499958+Omgaikwadb@users.noreply.github.com>
2024-10-09 23:19:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-function getHistory(){
-	return document.getElementById("history-value").innerText;
-}
-function printHistory(num){
-	document.getElementById("history-value").innerText=num;
-}
-function getOutput(){
-	return document.getElementById("output-value").innerText;
-}
-function printOutput(num){
-	if(num==""){
-		document.getElementById("output-value").innerText=num;
-	}
-	else{
-		document.getElementById("output-value").innerText=getFormattedNumber(num);
-	}	
-}
-function getFormattedNumber(num){
-	if(num=="-"){
-		return "";
-	}
-	var n = Number(num);
-	var value = n.toLocaleString("en");
-	return value;
-}
-function reverseNumberFormat(num){
-	return Number(num.replace(/,/g,''));
-}
 var operator = document.getElementsByClassName("operator");
 for(var i =0;i<operator.length;i++){
 	operator[i].addEventListener('click',function(){
@@ -38,6 +10,14 @@ for(var i =0;i<operator.length;i++){
 			if(output){//if output has a value
 				output= output.substr(0,output.length-1);
 				printOutput(output);
+			}
+		}
+		else if(this.id == "%"){ 
+			var output = getOutput();
+			if(output != ""){
+				output = reverseNumberFormat(output);
+				output = output / 100; // Convert the output to percentage
+				printOutput(output.toString());
 			}
 		}
 		else{
@@ -62,17 +42,6 @@ for(var i =0;i<operator.length;i++){
 					printOutput("");
 				}
 			}
-		}
-		
-	});
-}
-var number = document.getElementsByClassName("number");
-for(var i =0;i<number.length;i++){
-	number[i].addEventListener('click',function(){
-		var output=reverseNumberFormat(getOutput());
-		if(output!=NaN){ //if output is a number
-			output=output+this.id;
-			printOutput(output);
 		}
 	});
 }
